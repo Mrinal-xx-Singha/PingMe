826f32cca1c681156b5665684b26dbd703e61f45
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2026-09-26 15:07:26 +0000

    fix(responsiveness) alligned chatHeader buttons container and the sidebar for smaller devices

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,17 +16,20 @@ const HomePage = () => {
   },[subscribeToWatchParty,unsubscribeFromWatchParty ])
 
   return (
-    <div className="min-h-screen bg-base-200 py-20">
-      <div className="container mx-auto px-4 max-w-7xl">
-        <div className="bg-base-100 rounded-2xl shadow-cl w-full overflow-hidden">
-          <div className="flex h-[calc(100vh-10rem)] max-h-[900px] rounded-lg">
+    <div className="min-h-screen bg-base-200 pt-20">
+      <div className="container mx-auto px-0 lg:px-4 max-w-7xl h-[calc(100vh-5rem)]">
+        <div className="bg-base-100 lg:rounded-2xl rounded-2xl shadow-xl w-full overflow-hidden lg:h-auto">
+          <div className="flex h-full lg:h-[calc(100vh-10rem)] lg:max-h-[900px] rounded-lg">
            {/* Pass teh state as props to the sidebar  */}
+           {/* Lft side bar hidden on mobile when a chat is open */}
+           <div className={`${selectedUser ? 'hidden' : 'flex'} lg:flex`}>
             <Sidebar isCollapsed={isSidebarCollapsed}
             toggleCollapse={()=>setIsSidebarCollapsed(!isSidebarCollapsed)}
             />
+            </div>
             {/* This middle section will automatically grow when the sidebar shrinks */}
            
-           <div className="flex flex-1 flex-col min-w-0">
+           <div className={`${selectedUser ? 'flex' : 'hidden'} lg:flex flex-1 flex-col min-w-0`}>
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
            </div>
            <NewsSidebar />
